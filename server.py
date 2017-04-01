545776149b9161f499420cc2e3ff1d7b99329926
Joshua Liu <joshliu6830@gmail.com>
2017-04-01 18:55:55 -0400

    audio upload

--- server.py
+++ server.py
@@ -2,11 +2,12 @@ import os
 from flask import Flask, request, redirect, url_for
 from werkzeug.utils import secure_filename
 
-UPLOAD_FOLDER = '/Users/avery/Desktop/Mnemonic/uploads/'
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+UPLOAD_IMAGE_FOLDER = '/Users/josh/temp/Mnemonic/uploads/'
+ALLOWED_IMAGE_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+ALLOWED_AUDIO_EXTENSIONS = set(['wav', 'mp3'])
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['UPLOAD_IMAGE_FOLDER'] = UPLOAD_IMAGE_FOLDER
 
 
 # @app.route('/')
@@ -15,10 +16,10 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 def allowed_file(filename):
     return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_IMAGE_EXTENSIONS.union(ALLOWED_AUDIO_EXTENSIONS)
 
-@app.route('/', methods=['GET', 'POST'])
-def upload_file():
+@app.route('/upload/audio', methods=['GET', 'POST'])
+def upload_audio():
 	if request.method == 'POST':
 		# check if the post request has the file part
 		if 'file' not in request.files:
@@ -32,14 +33,42 @@ def upload_file():
 			return redirect(request.url)
 		if file and allowed_file(file.filename):
 			filename = secure_filename(file.filename)
-			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-			return redirect(url_for('uploaded_file', filename=filename))
+			file.save(os.path.join(app.config['UPLOAD_IMAGE_FOLDER'], filename))
+			return redirect(url_for('upload_audio', filename=filename))
 	return '''
 	<!doctype html>
-	<title>Upload new File</title>
-	<h1>Upload new File</h1>
+	<title>Upload new Audio File</title>
+	<h1>Upload new Audio File</h1>
 	<form method=post enctype=multipart/form-data>
 	  <p><input type=file name=file>
 		 <input type=submit value=Upload>
 	</form>
 	'''
+
+@app.route('/upload/image', methods=['GET', 'POST'])
+def upload_image():
+	if request.method == 'POST':
+		# check if the post request has the file part
+		if 'file' not in request.files:
+			flash('No file part')
+			return redirect(request.url)
+		file = request.files['file']
+		# if user does not select file, browser also
+		# submit a empty part without filename
+		if file.filename == '':
+			flash('No selected file')
+			return redirect(request.url)
+		if file and allowed_file(file.filename):
+			filename = secure_filename(file.filename)
+			file.save(os.path.join(app.config['UPLOAD_IMAGE_FOLDER'], filename))
+			return redirect(url_for('upload_image', filename=filename))
+	return '''
+	<!doctype html>
+	<title>Upload new Image</title>
+	<h1>Upload new Image</h1>
+	<form method=post enctype=multipart/form-data>
+	  <p><input type=file name=file>
+		 <input type=submit value=Upload>
+	</form>
+	'''
+	
