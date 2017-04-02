af1b318732da207c9e9e608e5705f90fa9ea4233
Avery Lamp <averylamp@gmail.com>
2017-04-02 05:54:36 -0400

    str

--- server.py
+++ server.py
@@ -281,10 +281,12 @@ def addPersonToDatabase(name, info):
 		'Content-Type': 'application/octet-stream',
 		'Ocp-Apim-Subscription-Key': '7c8d348d98b34b189840400a9ae58bcb',
 	}
-	body = "{\"name\":\"" + name + "\",\"userData\":\"" + info+ "\"\"}"
+	body = {}
+	body["name"] = name
+	body["userData"] = info
 	print ("Body - {}".format(body))
 	conn = httplib.HTTPSConnection('westus.api.cognitive.microsoft.com')
-	conn.request("POST", "/face/v1.0/persongroups/people/persons", body, headers)
+	conn.request("POST", "/face/v1.0/persongroups/people/persons", str(body), headers)
 	response = conn.getresponse()
 	data = response.read()
 	data_dict = ast.literal_eval(data)
