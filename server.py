2c4776ad76c50488b7d315288689c39eac64eb7f
Avery Lamp <averylamp@gmail.com>
2017-04-02 07:08:41 -0400

    convert to str

--- server.py
+++ server.py
@@ -27,6 +27,38 @@ UPLOAD_COUNT = 0
 # @app.route('/')
 # def hello_world():
 # 	return 'Hello, World!'
+def dict_to_string(json_dict):
+	json_dict = ast.literal_eval(json_dict)
+	string = ''
+	for i in json_dict:
+		string += i
+		string += ':'
+		if type(json_dict[i]) is list:
+			string += '['
+			for j in json_dict[i]:
+				string += j
+				string += ','
+			string = string[:-1]
+			string += ']'
+		elif type (json_dict[i]) is str:
+			string += json_dict[i]
+		string += ';'
+	string = string[:-1]
+	return string
+
+
+def string_to_dict(json_string):
+	diction = {}
+	elements = json_string.split(';')
+	for i in elements:
+		keys_values = i.split(':')
+		if keys_values[1][0] == '[':
+			temp = keys_values[1][1:-1]
+			objs = temp.split(',')
+			diction[keys_values[0]] = objs
+		else:
+			diction[keys_values[0]] = keys_values[1]
+	return str(diction)
 
 def allowed_file(filename):
     return '.' in filename and \
@@ -289,7 +321,7 @@ def addPersonToDatabase(name, info):
 	}
 	body = {}
 	body["name"] = name
-	body["userData"] = info
+	body["userData"] = dict_to_string(info)
 	print ("Body - {}".format(body))
 	conn = httplib.HTTPSConnection('westus.api.cognitive.microsoft.com')
 	conn.request("POST", "/face/v1.0/persongroups/people/persons", str(body), headers)
